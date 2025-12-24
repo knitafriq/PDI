7fdd7c5a2e886a608211be2176ae0b90b4d4aaff
unknown <Administrator@NO-TMatlhwane.salga.local>
2025-12-25 00:34:23 +0200

    fix: map sizes overlapping

--- src/components/ComplexityChoroplethMap.tsx
+++ src/components/ComplexityChoroplethMap.tsx
@@ -507,10 +507,10 @@ return (
 
       <MapContainer
         center={[-30, 24]}
-        zoom={isMobile ? 4.499 : 5}
+        zoom={isMobile ? 4.4995 : 5}
         maxBounds={isMobile ? undefined : bounds}
         maxBoundsViscosity={isMobile ? 0 : 1.0}
-        minZoom={isMobile ? 4.4 : 5}
+        minZoom={isMobile ? 4.4995 : 5}
         maxZoom={11}
         zoomSnap={0.001}     // ✅ CRITICAL
         zoomDelta={0.001}    // ✅ CRITICAL
