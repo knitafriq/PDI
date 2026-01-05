625c4dba2ab66cda4959f50ecaf2018821e2bf1c
unknown <Administrator@NO-TMatlhwane.salga.local>
2026-01-05 22:48:19 +0200

    Fixing the cards in profiles

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -530,7 +530,7 @@ const Profile: React.FC = () => {
               <div style={{maxWidth: isMobile ? 202 : 260,
                            marginTop: isMobile ? 12 : 0,
                            marginLeft: isMobile ? "auto" : 0,
-                           marginRight: isMobile ? 12 : 0,
+                           marginRight: isMobile ? 24 : 0,
                            position: isMobile ? "relative" : "static",
                            zIndex: isMobile ? 20 : "auto",
                            pointerEvents: "auto",
